36005baffbc0944aa34e2b786608c55a9eab248e
Clint.Network <me@clint.network>
2019-03-24 20:00:29 +0100

    Add new console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stormctl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
