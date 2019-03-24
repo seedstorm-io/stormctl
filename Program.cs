d7b5c3669008612a6ba72340101b5d2bcff076f5
Clint.Network <me@clint.network>
2019-03-24 20:31:47 +0100

    Implement Program Base

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
-﻿using Figgle;
-using System;
+﻿using System;
+
+/**
+  ____                _ ____  _                         _
+ / ___|  ___  ___  __| / ___|| |_ ___  _ __ _ __ ___   (_) ___
+ \___ \ / _ \/ _ \/ _` \___ \| __/ _ \| '__| '_ ` _ \  | |/ _ \
+  ___) |  __/  __/ (_| |___) | || (_) | |  | | | | | |_| | (_) |
+ |____/ \___|\___|\__,_|____/ \__\___/|_|  |_| |_| |_(_)_|\___/
+
+stormctl is the command line tool to manage your seedstorm.io account.
+
+**/
 
 namespace stormctl
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write(FiggleFonts.Standard.Render("SeedStorm.io"));
-            Console.WriteLine(" stormctl is the command line tool to manage your seedstorm.io account.");
-        }
+        public static int Main(string[] args) => new StormCtl(args).Banner().Execute();
     }
 }
